3804cca6e5dbcea91a0853feec5d228647501def
Tan Thinh <tanthinhvnex@gmail.com>
2023-12-20 22:49:24 +0700

    ex_6

---
+++ ex_6.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to find out
+//  if 1st January will be a Sunday between 2014 and 2050.
+
+let date = new Date("2014-01-01");
+for (let i = 2014; i <= 2050; i++) {
+    console.log(date.getFullYear());
+    console.log(date.getDay() === 0 ? "Yes" : "No");
+    date.setFullYear(i + 1);
+}
